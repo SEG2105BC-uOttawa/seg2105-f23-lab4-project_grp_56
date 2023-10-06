7b802c7112a8cbe1840873f4d33c1cc8bf4998ad
yasnakj <145388006+yasnakj@users.noreply.github.com>
2023-10-06 17:26:51 -0400

    Initial commit

---
+++ app/src/main/java/com/uottawa/lab4/MainActivity.java
@@ -0,0 +1,69 @@
+package com.uottawa.lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void OnOpenInGoogleMaps (View view) {
+        EditText teamAddres = (EditText) findViewById(R.id.teamAddressField);
+        // Create a Uri from an intent string. Use the result to create an Intent.
+        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q=" + teamAddres.getText());
+        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        // Make the Intent explicit by setting the Google Maps package
+        mapIntent.setPackage("com.google.android.apps.maps");
+        // Attempt to start an activity that can handle the Intent
+        startActivity(mapIntent);
+    }
+
+    public void OnSetAvatarButton(View view) {
+        //Application Context and Activity
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivityForResult (intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) return;
+//Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.logoImage);
+//Figuring out the correct image
+        String drawableName = "ic_logo_00";
+        int imageID = data.getIntExtra("imageID", R.id.logoImage00);
+        if (imageID == R.id.logoImage00) {
+            drawableName = "ic_logo_00";
+        } else if (imageID == R.id.logoImage01) {
+            drawableName = "ic_logo_01";
+        } else if (imageID == R.id.logoImage02) {
+            drawableName = "ic_logo_02";
+        } else if (imageID == R.id.logoImage03) {
+            drawableName = "ic_logo_03";
+        } else if (imageID == R.id.logoImage04) {
+            drawableName = "ic_logo_04";
+        } else if (imageID == R.id.logoImage05) {
+            drawableName = "ic_logo_05";
+        } else {
+            drawableName = "ic_logo_00";
+        }
+    }
+}
